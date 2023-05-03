a02bdba0b4bb54dba6b4d2f6c809ea4bd1813046
Maksymilian Walicki <maxwalicki@gmail.com>
2023-05-03 16:45:50 +0200

    ADD - handle login redirect

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -33,7 +33,7 @@ const App = () => {
           <Hero />
           <About />
           <MailForm />
-          <Reviews />
+          <Reviews /> c  
           <Newsletter />
           <Footer />
         </>
