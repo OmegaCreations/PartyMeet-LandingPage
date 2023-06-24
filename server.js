67c9de66b4edfa09bedd444cb3ab1e1a65889c91
Maksymilian Walicki <maxwalicki@gmail.com>
2023-06-24 17:39:15 +0200

    set to production mode

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv'
 dotenv.config();
 
 // Constants
-const isProduction = false
+const isProduction = true
 const port = process.env.PORT || 5173
 const base = process.env.BASE || '/'
 
