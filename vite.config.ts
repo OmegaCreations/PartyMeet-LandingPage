6681da9638a8588942b4052f0afe511ff6ceda58
Maksymilian Walicki <maxwalicki@gmail.com>
2023-06-24 18:19:28 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      // with RegEx: http://localhost:5173/fallback/ -> http://jsonplaceholder.typicode.com/
+      '^/fallback/.*': {
+        target: 'http://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/fallback/, ''),
+      },
+    },
+  },
 })
